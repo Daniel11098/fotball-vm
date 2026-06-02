7247593d441462d3009309a82863bcaa35539249
Daniel11098 <danielaz@hiof.no>
2026-06-02 18:26:21 +0200

    Spring Boots setup og kobling til GitHub

---
+++ fotball-vm/src/main/java/no/vm2026/fotball_vm/FotballVmApplication.java
@@ -0,0 +1,13 @@
+package no.vm2026.fotball_vm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotballVmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotballVmApplication.class, args);
+	}
+
+}
---
+++ fotball-vm/src/test/java/no/vm2026/fotball_vm/FotballVmApplicationTests.java
@@ -0,0 +1,13 @@
+package no.vm2026.fotball_vm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FotballVmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
